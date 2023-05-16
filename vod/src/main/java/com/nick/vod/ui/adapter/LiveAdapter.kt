8a341f9aba28c1c76c575c9267e6128c24824c96
HannahNick <365719562@qq.com>
2023-05-16 17:16:18 +0800

    1.新增直播列表

---
+++ vod/src/main/java/com/nick/vod/ui/adapter/LiveAdapter.kt
@@ -0,0 +1,12 @@
+package com.nick.vod.ui.adapter
+
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.viewholder.BaseViewHolder
+import com.nick.base.vo.MusicVo
+import com.nick.vod.R
+
+class LiveAdapter: BaseQuickAdapter<MusicVo,BaseViewHolder>(R.layout.adapter_live_item) {
+    override fun convert(holder: BaseViewHolder, item: MusicVo) {
+        holder.setText(R.id.tv_name,item.liveName)
+    }
+}
--- vod/src/main/java/com/nick/vod/ui/fragment/VodFragment.kt
+++ vod/src/main/java/com/nick/vod/ui/fragment/VodFragment.kt
@@ -11,11 +11,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.blankj.utilcode.util.LogUtils
-import com.blankj.utilcode.util.PathUtils
-import com.blankj.utilcode.util.ScreenUtils
 import com.blankj.utilcode.util.ServiceUtils
-import com.nick.base.BaseUrl
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.listener.OnItemClickListener
 import com.nick.base.vo.MusicVo
 import com.nick.base.vo.enum.UrlType
 import com.nick.music.entity.PlayInfo
@@ -23,8 +23,7 @@ import com.nick.music.player.PlayInfoCallBack
 import com.nick.music.server.MusicServer
 import com.nick.music.server.binder.MusicBinder
 import com.nick.vod.databinding.LayoutLiveBinding
-import com.nick.vod.view.LiveGestureControlLayer
-import java.io.File
+import com.nick.vod.ui.adapter.LiveAdapter
 import java.util.*
 
 class VodFragment: Fragment(), ServiceConnection, PlayInfoCallBack, SurfaceHolder.Callback{
@@ -32,6 +31,7 @@ class VodFragment: Fragment(), ServiceConnection, PlayInfoCallBack, SurfaceHolde
     private val mBindingView by lazy { LayoutLiveBinding.inflate(layoutInflater) }
     private val mTasks: Queue<Runnable> = LinkedList()
     private lateinit var mMusicBinder: MusicBinder
+    private val mLiveAdapter = LiveAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -59,12 +59,36 @@ class VodFragment: Fragment(), ServiceConnection, PlayInfoCallBack, SurfaceHolde
 //        LogUtils.i("电影本地路径:${PathUtils.getExternalMoviesPath()}/bear.mp4  isExists: ${File(PathUtils.getExternalMoviesPath()+"/bear.mp4").exists()}")
         val initDataTask = Runnable {
             val data = listOf(
-//                MusicVo("1","bear","bear","path","movies/bear.mp4","123","123"),
-//                MusicVo("1","bear","bear","play/QbYVXQpd/index.m3u8",UrlType.M3U8,"123","123","123"),
                 MusicVo(path = "http://ali-vl.cztv.com/channels/lantian/channel001/360p.m3u8?a=1000&d=2e81e5ce97d2a771861cbe3b0c492876&k=edb9ed932efb100c8acc51590186e08e&t=1684153870", pathType = UrlType.M3U8, liveName = "浙江卫视"),
+                MusicVo(path = "https://padtx.qing.mgtv.com/nn_live/nn_x64/cWlkPSZzPTQzZjQyOWI3ZTMzZTNiNzVlMTFjODg2ZDNjODhlYzMxJmVzPTE2ODQyNTQzMTgmdXVpZD0wYjJjMTExMTU1YTg5ZmU0ZGZhYjU4NDc3NDE0YzFiOS03MTU5MGJlMiZ2PTImYXM9MCZjZG5leF9pZD10eF9waG9uZV9saXZl/HNYLMPP360.m3u8?_t=1684225517852&_t=1684225523139", pathType = UrlType.M3U8, liveName = "芒果娱乐"),
+                MusicVo(path = "https://sztv-live.cutv.com/AxeFRth/500/q7Ave0R.m3u8?sign=fb88623ccabc098eb19d4261e79b6374&t=646361cf", pathType = UrlType.M3U8, liveName = "深圳卫视"),
+                MusicVo(path = "https://sztv-live.cutv.com/ZwxzUXr/500/x1uZvx0.m3u8?sign=b2dfe34eb4dd01f11e36b24a14988d23&t=6463623a", pathType = UrlType.M3U8, liveName = "深圳都市频道"),
+                MusicVo(path = "https://sztv-live.cutv.com/2q76Sw2/500/d3k2v70.m3u8?sign=72d150bdea78411548ac66ae3581804f&t=64636276", pathType = UrlType.M3U8, liveName = "深圳公共频道"),
+                MusicVo(path = "https://sztv-live.cutv.com/1q4iPng/500/f2i1vp0.m3u8?sign=947d87a69144f0873c6257794f32a9dd&t=646362a8", pathType = UrlType.M3U8, liveName = "深圳娱乐频道"),
+                MusicVo(path = "https://sztv-live.cutv.com/4azbkoY/500/6rpvz0k.m3u8?sign=9c78eb218ba7010cdcde737c6c578dd2&t=646362ce", pathType = UrlType.M3U8, liveName = "深圳电视剧频道"),
+                MusicVo(path = "https://sztv-live.cutv.com/sztvgjpd/500/a77svt0g.m3u8?sign=9bc0d6a5446282926ba28053fed148be&t=646362f8", pathType = UrlType.M3U8, liveName = "深圳国际频道"),
+                MusicVo(path = "https://volc-stream.kksmg.com/live/dfws/index.m3u8?volcSecret=4f341cef920f83557063876cdf400f28&volcTime=1684312287", pathType = UrlType.M3U8, liveName = "东方卫视"),
+                MusicVo(path = "https://tencent-stream.kksmg.com/live/ylpd.m3u8?txSecret=86dfe8f9d8ac20d2a500f3b28106f5c6&txTime=6464912f", pathType = UrlType.M3U8, liveName = "都市频道"),
+                MusicVo(path = "https://tencent-stream.kksmg.com/live/wypd.m3u8?txSecret=d431af9912bf409330443a0abd89a54f&txTime=64649158", pathType = UrlType.M3U8, liveName = "外语频道"),
 
             )
             mMusicBinder.setPlayList(data)
+            mBindingView.rvLive.apply {
+                layoutManager = LinearLayoutManager(context)
+                mLiveAdapter.addData(data)
+                adapter = mLiveAdapter
+                mLiveAdapter.setOnItemClickListener(object : OnItemClickListener {
+                    override fun onItemClick(
+                        adapter: BaseQuickAdapter<*, *>,
+                        view: View,
+                        position: Int
+                    ) {
+                        mMusicBinder.play(position)
+                        mBindingView.gcLayer.setLiveName(mMusicBinder.getPlayInfo().liveName)
+                    }
+
+                })
+            }
         }
         val registerCallBackTask = Runnable {
             mMusicBinder.registerCallBack(this)
